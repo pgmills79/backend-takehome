b6eaa8f198e33f62a2e49c9101c69c1286eed5fa
Paul Mills <Mills.Paul@mayo.edu>
2021-12-29 13:40:23 -0500

    added:  swagger for testing if no postman

--- RatingEngine/RatingEngine.API/Startup.cs
+++ RatingEngine/RatingEngine.API/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 
 namespace RatingEngine
@@ -27,6 +28,11 @@ namespace RatingEngine
             {
                 options.Filters.Add(new ProducesAttribute("application/json"));
             });
+            
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {Title = "Quote Rating Engine API", Version = "v1"});
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +41,8 @@ namespace RatingEngine
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Quote Rating Engine API v1"));
             }
 
             app.UseHttpsRedirection();
